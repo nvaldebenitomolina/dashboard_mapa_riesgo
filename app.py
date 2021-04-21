b67a62a4a92c9655eb65a41db7827c1088d2a3a4
Nancy Valdebenito <nvaldebenito@uchile.cl>
2021-04-21 17:19:10 -0400

    add password user

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template
 from flask_jsglue import JSGlue
 
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+users = {
+    "cr2dgf": generate_password_hash("wpcr2")
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
 jsglue = JSGlue()
 app = Flask(__name__)
 jsglue.init_app(app)
 
 @app.route('/')
+@auth.login_required
 
 
 def home():
